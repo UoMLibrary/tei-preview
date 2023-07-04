4c70e75970b79bd037b73d499d73f0b09b7e6e34
Christopher Wilson <chris@abitofcode.com>
2023-07-04 11:58:35 +0100

    Added an API route to convert XSLT strings to .sef format

---
+++ src/routes/api/sef/+server.js
@@ -0,0 +1,12 @@
+import saxon from 'saxon-js';
+
+export const POST = async ({ request, params }) => {
+	const xslString = await request.text();
+	// Compile the supplied XSL String into a .sef file
+	let env = saxon.getPlatform();
+	let doc = env.parseXmlFromString(xslString);
+	// hack: avoid error "Required cardinality of value of parameter $static-base-uri is exactly one; supplied value is empty"
+	doc._saxonBaseUri = 'file:///';
+	const sef = saxon.compile(doc);
+	return new Response(JSON.stringify({ status: 'success', sef }));
+};
