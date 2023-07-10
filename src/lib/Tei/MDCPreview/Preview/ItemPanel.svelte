72eb0772ec20c83b567f343f9d5c0a0a9f2d6728
Christopher Wilson <chris@abitofcode.com>
2023-07-10 21:59:57 +0100

    Stubbed out itempanel panels

--- src/lib/Tei/MDCPreview/Preview/ItemPanel.svelte
+++ src/lib/Tei/MDCPreview/Preview/ItemPanel.svelte
@@ -1,5 +1,10 @@
 <script>
 	import ItemPanelTabs from './ItemPanel/ItemPanelTabs.svelte';
+	import About from './ItemPanel/About.svelte';
+	import Content from './ItemPanel/Content.svelte';
+	import Thumbnails from './ItemPanel/Thumbnails.svelte';
+	import Metadata from './ItemPanel/Metadata.svelte';
+	import ActionPanel from './ItemPanel/ActionPanel.svelte';
 
 	export let viewModel;
 	export let page;
@@ -8,6 +13,20 @@
 	let activeItem = 'About';
 </script>
 
-<aside class="flex flex-col">
+<aside class="flex flex-col h-full">
 	<ItemPanelTabs {activeItem} {tabItems} on:tabChange={(e) => (activeItem = e.detail)} />
+	<div class="flex-1 bg-white">
+		{#if activeItem == 'About'}
+			<About metadata={viewModel.about} />
+		{:else if activeItem == 'Contents'}
+			<Content contents={viewModel.contents} on:updatePage />
+		{:else if activeItem == 'Thumbnails'}
+			<Thumbnails pages={viewModel.thumbnails} on:updatePage />
+		{:else if activeItem == 'Metadata'}
+			<Metadata metadata={viewModel.metadata} />
+		{:else}
+			<div class="text-xs"><p class="flex-1">Under construction...</p></div>
+		{/if}
+	</div>
+	<ActionPanel />
 </aside>
---
+++ src/lib/Tei/MDCPreview/Preview/ItemPanel/ActionPanel.svelte
@@ -0,0 +1,8 @@
+<ul class="flex justify-between border-t-[1px]">
+	<li class="text-xs text-center p-2">Download options</li>
+	<li class="text-xs text-center p-2">Rights options</li>
+	<li class="text-xs text-center p-2">Download metadata options</li>
+	<li class="text-xs text-center p-2">Bookmark option</li>
+	<li class="text-xs text-center p-2">iiif option</li>
+	<li class="text-xs text-center p-2">Mirador option</li>
+</ul>
