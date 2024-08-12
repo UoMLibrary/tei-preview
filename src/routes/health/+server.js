8952accabe4310437685bb7802c6758e2c97851f
Christopher Wilson <chris@abitofcode.com>
2024-08-12 13:21:16 +0100

    Fix Github action and version in health

--- src/routes/health/+server.js
+++ src/routes/health/+server.js
@@ -1,6 +1,7 @@
 import { json } from '@sveltejs/kit';
+import { version } from './package.json';
 
 export function GET() {
-	let obj = { status: 'success', data: { created: new Date() } };
+	let obj = { status: 'success', data: { created: new Date(), version } };
 	return json(obj);
 }
