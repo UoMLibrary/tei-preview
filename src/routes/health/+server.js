3cabe8c51f1152b8ee3790bb80ee3e0bdac10061
Christopher Wilson <chris@abitofcode.com>
2024-08-12 13:44:56 +0100

    Audit fix + updated version to 1.0.1

--- src/routes/health/+server.js
+++ src/routes/health/+server.js
@@ -1,6 +1,6 @@
 import { json } from '@sveltejs/kit';
 
 export function GET() {
-	let obj = { status: 'success', data: { created: new Date(), version: '1.0.0' } };
+	let obj = { status: 'success', data: { created: new Date(), version: '1.0.1' } };
 	return json(obj);
 }
